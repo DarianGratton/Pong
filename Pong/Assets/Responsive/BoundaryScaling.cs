b8406e59309d9468065f0d9b1d9f3d9b99ebef08
Darian Gratton <dariangratton@gmail.com>
2019-09-15 17:32:46 -0700

    Added reponsive scaling for the boundarys

--- Pong/Assets/Responsive/BoundaryScaling.cs
+++ Pong/Assets/Responsive/BoundaryScaling.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class BoundaryScaling : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        var width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
+        var scale = 1.4;
+
+        var x = (float)(width * scale);
+        var y = transform.localScale.y;
+        var z = transform.localScale.z;
+
+        transform.localScale = new Vector3(x, y, z);       
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        
+    }
 }
---
+++ Pong/Assets/Responsive/PlayerBoundaryScaling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoundaryScaling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        var width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
+        var scale = transform.localPosition.x > 0 ? 1.46 : -1.46;
+
+        var x = (float)(width / scale);
+        var y = transform.localPosition.y;
+        var z = transform.localPosition.z;
+
+        transform.localPosition = new Vector3(x, y, z);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
