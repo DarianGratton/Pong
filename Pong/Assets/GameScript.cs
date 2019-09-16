b1afeb43c9ee1a328768a0a2a4df44b4687514b8
Darian Gratton <dariangratton@gmail.com>
2019-09-16 01:00:53 -0700

    Implemented scoring system without collision yet

---
+++ Pong/Assets/GameScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+    public GameObject HUD;
+    private HUDManager hudManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        hudManager = HUD.GetComponent<HUDManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        hudManager.score++;
+        hudManager.playerScored = true;
+	}
+}
---
+++ Pong/Assets/HUDManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+
+    Text HUDText;
+    public int score = 0;
+    public bool playerScored = true;
+    private string oriString;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        HUDText = GetComponent<Text>();
+        oriString = HUDText.text.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (playerScored)
+        {
+            HUDText.text = oriString + score.ToString();
+            playerScored = !playerScored;
+        }
+	}
+}
