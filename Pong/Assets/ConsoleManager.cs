8edec925f8e8bf410ab7c33aa406c33228235427
Darian Gratton <dariangratton@gmail.com>
2019-09-26 23:58:54 -0700

    Able to change backgroundcolor from console

--- Pong/Assets/ConsoleManager.cs
+++ Pong/Assets/ConsoleManager.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConsoleManager : MonoBehaviour {
 
     private Canvas console;
+    private Camera camera;
 
 	// Use this for initialization
 	void Start ()
     {
+        var input = GameObject.Find("ConsoleField").GetComponent<InputField>();
+        var se = new InputField.SubmitEvent();
+        input.onEndEdit.AddListener(SubmitCommand);
+
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
         console = GameObject.Find("Console").GetComponent<Canvas>();
 
         CloseTheConsole();
@@ -26,5 +33,33 @@ public class ConsoleManager : MonoBehaviour {
     public void CloseTheConsole()
     {
         console.enabled = false;
+    }
+
+    public void SubmitCommand(string command)
+    {
+        switch (command.ToLower())
+        {
+            case "red":
+                camera.backgroundColor = Color.red;
+                break;
+            case "blue":
+                camera.backgroundColor = Color.blue;
+                break;
+            case "cyan":
+                camera.backgroundColor = Color.cyan;
+                break;
+            case "gray":
+                camera.backgroundColor = Color.gray;
+                break;
+            case "green":
+                camera.backgroundColor = Color.green;
+                break;
+            case "magenta":
+                camera.backgroundColor = Color.magenta;
+                break;
+            case "yellow":
+                camera.backgroundColor = Color.yellow;
+                break;
+        }
     }
 }
